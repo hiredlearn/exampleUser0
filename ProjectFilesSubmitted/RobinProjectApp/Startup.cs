026349198f775ea39a7bf7aebdd2a5ed7b1516e4
John Doe <john@doe.org>
2021-01-26 17:25:43 -0800

    copy

---
+++ ProjectFilesSubmitted/RobinProjectApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RobinProjectApp.Startup))]
+namespace RobinProjectApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
